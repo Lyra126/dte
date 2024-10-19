6ea8d0f84dc35ca090b5ee51c49a5f0674d1f92d
awu1130 <66101418+awu1130@users.noreply.github.com>
2024-10-19 16:06:22 -0400

    Create geminiAI.js

---
+++ geminiAI.js
@@ -0,0 +1,71 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+async function generateMeditation(prompt) {
+  //const dotenv = require("dotenv");
+  //dotenv.config();
+  const API_KEY = 'AIzaSyCpjgXeQKRUNwEJHBWUTNN_WJcxIlyuZsA';
+  // Access API key as an environment variable (see "Set up your API key" above)
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  
+  const request = "Use information: " + prompt + " to suggest meditation routine in the context of postpartum. Do not start with a title, go straight into the meditation routine.";
+  
+  const result = await model.generateContent(request);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+}
+
+async function generateJournal(prompt) {
+  //const dotenv = require("dotenv");
+  //dotenv.config();
+  const API_KEY = 'AIzaSyCpjgXeQKRUNwEJHBWUTNN_WJcxIlyuZsA';
+  // Access API key as an environment variable (see "Set up your API key" above)
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  
+  const request = "Use information: " + prompt + " to suggest a helpful journaling prompt in the context of postpartum";
+  
+  const result = await model.generateContent(request);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+}
+
+async function generateExercise(prompt) {
+  //const dotenv = require("dotenv");
+  //dotenv.config();
+  const API_KEY = 'AIzaSyCpjgXeQKRUNwEJHBWUTNN_WJcxIlyuZsA';
+  // Access API key as an environment variable (see "Set up your API key" above)
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  
+  const request = "Use information: " + prompt + " to suggest a exercises in the context of postpartum";
+  
+  const result = await model.generateContent(request);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+}
+
+async function generateSleep(prompt) {
+  //const dotenv = require("dotenv");
+  //dotenv.config();
+  const API_KEY = 'AIzaSyCpjgXeQKRUNwEJHBWUTNN_WJcxIlyuZsA';
+  // Access API key as an environment variable (see "Set up your API key" above)
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  
+  const request = "Use information: " + prompt + " to suggest help for sleeping in the context of postpartum";
+  
+  const result = await model.generateContent(request);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+}
+
+export default generateExercise;
