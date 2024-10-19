75970235b4d7f13519f3633ea3f3c9a0d3b2fe46
HuangCher <165945110+HuangCher@users.noreply.github.com>
2024-10-19 15:44:00 -0400

    update

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
     email_address: { type: String, required: true },
     name: { type: String, required: false },
     user_name: { type: String, required: false },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    entries: { type: [String], required: false },
 }); 
 
 const UserModel = mongoose.model('User', UserSchema);
